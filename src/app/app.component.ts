5f38b202da4a7392403aef57ab0f37d25b48ec08
Axel Alfredo Sanchez Ortega <proaxel1214@gmail.com>
2024-11-11 19:30:21 -0600

    final revisado pizzeria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+/* import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,28 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'pizzeria';
-}
+} */
+
+  import { Component } from '@angular/core';
+  import { TablaPedidosComponent } from './tabla-pedidos/tabla-pedidos.component';
+  import { FormVentasComponent } from './form-ventas/form-ventas.component';
+  import { ResumenVentasComponent } from './resumen-ventas/resumen-ventas.component';
+  
+  @Component({
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+      TablaPedidosComponent,
+      FormVentasComponent,
+      ResumenVentasComponent,
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+  })
+  export class AppComponent {
+    title = 'pizzeria';
+  }
+  
+
+  
+  
